ad4dc6ad993564ebbedaa6cbb5dd43608437ab10
Chris Bannister <c.bannister@gmail.com>
2013-07-10 21:00:44 +0100

    Add redisQ and return a deferred object from getFeedLatest

--- app/src/server/redis.js
+++ app/src/server/redis.js
@@ -1,8 +1,22 @@
 var redis = require("redis").createClient()
 var Q = require("Q")
 
+var redisQ = function(defer) {
+	return function(err, res) {
+		if(err) {
+			return defer.reject(err)
+		}
+
+		return defer.resolve(res)
+	}
+}
+
 module.exports.setFeedLatest = function(channel, modified) {
-	redis.hset("feed.modified", channel, modified)
+	var defer = Q.defer()
+
+	redis.hset("feed.modified", channel, modified, redisQ(defer))
+
+	return defer.promise
 }
 
 module.exports.getFeedLatest = function(channel) {
